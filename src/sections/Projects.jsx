be248849a7609767fe4db5da47b1355ffa433d9e
nataly-tuong <nataly-tuong@users.noreply.github.com>
2025-08-14 20:54:14 -0700

    projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -12,6 +12,36 @@ export default function Projects() {
         top-0 left-0 w-full h-40 absolute z-10"
         ></div>
         <h3 className="text-3xl font-bold z-60 mb-6 relative">Projects</h3>
+
+        <div class="relative">
+          <div
+            class="bg-black
+        bg-[url('/Texturelabs_InkPaint_131S.jpg')] bg-[position:10%_center] bg-[size:250%] w-90 h-70
+        border-2 border-mainAccent relative z-4 shadow-[15px_0_15px_rgba(0,0,0,0.5)]"
+          >
+            <div
+              class="bg-black h-50 w-85 top-2 left-2 
+            absolute z-3
+            bg-[url('/santa_monica_sunsets.png')] bg-cover bg-[position:center_3%]"
+            ></div>
+            <h4 class="z-5 absolute text-2xl font-semibold top-55 left-5">
+              Santa Monica Sunsets
+            </h4>
+          </div>
+          <div
+            class="bg-amber-100
+        bg-[url('/Texturelabs_Paper_320S.jpg')] [size:250%] bg-center rounded-2xl
+        h-60 w-60 absolute z-2 top-5 left-70"
+          >
+            <div
+              class="absolute z-11 left-24 top-3 text-black
+             bg-transparent h-55 w-33 text-wrap"
+            >
+              Designed and developed a fictional responsive agency website using
+              HTML5 and CSS3 with mobile-first design
+            </div>
+          </div>
+        </div>
       </div>
     </section>
   );
